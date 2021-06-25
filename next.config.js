139f02b7c68012256234cb89c40425cd3a81b9de
asiwajudev <olanrewajuranda@gmail.com>
2021-06-25 21:52:47 +0100

    Configuration to deploy on Netlify added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-// module.exports = {
-//   reactStrictMode: true,
-//   distDir: "out",
-// };
-// next.config.js
 module.exports = {
-  // Target must be serverless
-  target: "serverless",
+  reactStrictMode: true,
 };
